30a052d90c97ea9e31bc0939a965744334ad57d7
YevhenKoss <kossik89@gmail.com>
2023-04-29 19:09:43 +0300

    decorated input fields

--- hw/hw/settings.py
+++ hw/hw/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_quotes',
-    'app_users'
+    'app_users',
+#     'crispy_forms'
+#     'crispy_bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,7 @@ LOGIN_URL = '/users/signin'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CRISPY_TEMPLATE_PACK = 'bootstrap4'
+#
+# CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
